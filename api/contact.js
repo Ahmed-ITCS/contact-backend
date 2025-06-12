6dcbd89987199835a4f467718be9f4a50041206e
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-06-12 04:48:44 +0300

    cors

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,8 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
